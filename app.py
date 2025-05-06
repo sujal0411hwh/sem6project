e478d05da9dac8f6df893d1ec8eee49ab2260202
Sujal Vachhani <vachhanisujal4@gmail.com>
2025-05-06 12:51:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -173,4 +173,5 @@ def contactus():
 
 # --- RUN SERVER ---
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT env var if available
+    app.run(host="0.0.0.0", port=port)
